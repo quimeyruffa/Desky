d94f18b11800c61831cbd2aa1b38f64b9d5365ff
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2021-10-09 20:16:52 -0300

    Componente Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Search } from './components/Search/Search';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+     <Navbar />
      <Search/>
     </div>
   );
