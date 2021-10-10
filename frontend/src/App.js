f739e91f7f73d5968ee69e0bf0883c89daebf876
quimey <quimeyzoe@hotmail.com>
2021-10-10 19:19:10 -0300

    new footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Footer } from './components/Footer/Footer';
 import { Search } from './components/Search/Search';
 
 function App() {
   return (
     <div className="App">
-     <Search/>
+     <Search />
+     <Footer />
     </div>
   );
 }
