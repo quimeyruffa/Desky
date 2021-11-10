1086e63da81cf70543c91646f7fa14b75c415f30
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2021-11-09 21:46:21 -0300

    some changes

--- frontend/src/components/SearchComponents/Busqueda.js
+++ frontend/src/components/SearchComponents/Busqueda.js
@@ -13,170 +13,10 @@ import {DropdwOffice} from "./DropdwOffice/DropdwOffice";
 import { useHistory } from "react-router";
 
 export const Busqueda = () => {
-    const [data, setData] = useState ([]);
     const history = useHistory()
-    const estado = history.location.state;
-    
+    const uri = 'http://localhost:3000';
 
-    const [datos, setDatos] = useState([
-        {
-            "_id": "001",
-            "nombre": "weWork",
-            "email": "weworka@gmail.com",
-            "resena": [
-                {
-                    "_id": "61732c0d7ee335f8f6c7c8a1",
-                    "puntaje": 4,
-                    "nombre": "julio",
-                    "texto": "blablabla",
-                    "fecha": "2021-10-14T00:00:00.000Z"
-                }
-            ],
-            "direccion": {
-                "_id": "61732c0d7ee335f8f6c7c8a2",
-                "streetAddress": "126 Udhna",
-                "city": "san jone",
-                "state": "CA",
-                "postalCode": "394221",
-                "Coordinate": "-34.617363885768896, -58.38180343127131"
-            },
-            "promedioPuntos": 3.2,
-            "tipo": [
-                {
-                    "_id": "61732c0d7ee335f8f6c7c8a3",
-                    "type": "openOffice",
-                    "precio": 10000,
-                    "fechaIni": "2021-10-14T00:00:00.000Z",
-                    "fechaFin": "2021-10-14T00:00:00.000Z",
-                    "cantPersonas": 3,
-                    "diferencial": [
-                        "parking",
-                        "petFriendly",
-                        "podcast"
-                    ],
-                    "reserva": [
-                        {
-                            "_id": "61732c0d7ee335f8f6c7c8a4",
-                            "idUsuario": "001"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "_id": "002",
-            "nombre": "HIT Cowork",
-            "email": "hitcowork@gmail.com",
-            "resena": [
-                {
-                    "_id": "61732c0d7ee335f8f6c7c8a5",
-                    "puntaje": 3.5,
-                    "nombre": "Carlos",
-                    "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget faucibus justo, at semper odio. Phasellus quis ipsum augue. Sed rutrum, felis eu varius mattis,",
-                    "fecha": "2021-10-14T00:00:00.000Z"
-                },
-                {
-                    "_id": "61732c0d7ee335f8f6c7c8a6",
-                    "puntaje": 3.5,
-                    "nombre": "Carlos",
-                    "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget faucibus justo, at semper odio. Phasellus quis ipsum augue. Sed rutrum, felis eu varius mattis,",
-                    "fecha": "2021-10-14T00:00:00.000Z"
-                }
-            ],
-            "direccion": {
-                "_id": "61732c0d7ee335f8f6c7c8a7",
-                "streetAddress": "1391 La Pampa",
-                "city": "CABA",
-                "state": "Buenos Aires",
-                "postalCode": "C1428DZA",
-                "Coordinate": "-34.55855263802333, -58.44414754399354"
-            },
-            "promedioPuntos": 3.5,
-            "tipo": [
-                {
-                    "_id": "61732c0d7ee335f8f6c7c8a8",
-                    "type": "openOffice",
-                    "precio": 20000,
-                    "fechaIni": "2021-10-14T00:00:00.000Z",
-                    "fechaFin": "2021-10-14T00:00:00.000Z",
-                    "cantPersonas": 30,
-                    "diferencial": [
-                        "petFriendly",
-                        "kitchen",
-                        "parking"
-                    ],
-                    "reserva": [
-                        {
-                            "_id": "61732c0d7ee335f8f6c7c8a9",
-                            "idUsuario": "001"
-                        }
-                    ]
-                },
-                {
-                    "_id": "61732c0d7ee335f8f6c7c8aa",
-                    "reserva": [],
-                    "type": "privateOffice",
-                    "precio": 11000,
-                    "fechaIni": "2021-10-14T00:00:00.000Z",
-                    "fechaFin": "2021-10-14T00:00:00.000Z",
-                    "cantPersonas": 30,
-                    "diferencial": [
-                        "dining"
-                    ]
-                }
-            ]
-        },
-        {
-            "_id": "003",
-            "nombre": "LA COMU Cowork",
-            "email": "lacomucowork@gmail.com",
-            "resena": [
-                {
-                    "_id": "61732c0d7ee335f8f6c7c8ab",
-                    "puntaje": 3.5,
-                    "nombre": "Carlos",
-                    "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget faucibus justo, at semper odio. Phasellus quis ipsum augue. Sed rutrum, felis eu varius mattis,",
-                    "fecha": "2021-10-14T00:00:00.000Z"
-                },
-                {
-                    "_id": "61732c0d7ee335f8f6c7c8ac",
-                    "puntaje": 5,
-                    "nombre": "Enriqueta",
-                    "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget faucibus justo, at semper odio. Phasellus quis ipsum augue. Sed rutrum, felis eu varius mattis,",
-                    "fecha": "2021-10-14T00:00:00.000Z"
-                }
-            ],
-            "direccion": {
-                "_id": "61732c0d7ee335f8f6c7c8ad",
-                "streetAddress": "2240 San Lorenzo",
-                "city": "PBA",
-                "state": "Buenos Aires",
-                "postalCode": "B1636BVP",
-                "Coordinate": "-34.50716172844765, -58.5015953920664"
-            },
-            "promedioPuntos": 4.25,
-            "tipo": [
-                {
-                    "_id": "61732c0d7ee335f8f6c7c8ae",
-                    "type": "openOffice",
-                    "precio": 7000,
-                    "fechaIni": "2021-10-14T00:00:00.000Z",
-                    "fechaFin": "2021-10-14T00:00:00.000Z",
-                    "cantPersonas": 30,
-                    "diferencial": [
-                        "parking",
-                        "coffee"
-                    ],
-                    "reserva": [
-                        {
-                            "_id": "61732c0d7ee335f8f6c7c8af",
-                            "idUsuario": "001"
-                        }
-                    ]
-                }
-            ]
-        }
-    ]);
+    const [datos, setDatos] = useState([]);
     const [valueLlegada, setValueLlegada] = useState(new Date());
     const [valueName, setValueName] = useState("");
     const [valueSalida, setValueSalida] = useState(new Date());
@@ -191,6 +31,32 @@ export const Busqueda = () => {
     });
     const [miembros, setMiembros] = useState(0);
     const [oficina, setOficina] = useState("Cualquiera");
+    const [nombre, setNombre] = useState("");
+
+    useEffect(() => {
+        setNombre(localStorage.getItem("nombre"));
+        setValueLlegada(new Date(localStorage.getItem("fechaIni")));
+        setValueSalida(new Date(localStorage.getItem("fechaFin")));
+
+        if (nombre !== "") {
+
+        } else {
+            fetch(`${uri}/coworks`, {
+                method: 'GET',
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    setDatos(data);
+                    console.log(data);
+                }).catch(e => {
+                    console.log(e);
+            })
+
+        }
+    },[])
+
+    console.log(nombre, valueSalida, valueLlegada);
+
 
     const handleChangePrecios = (event, newValue) => {
         setRangoPrecios(newValue);
@@ -219,7 +85,8 @@ export const Busqueda = () => {
         setMiembros(miembros);
         setOficina(oficina);
     }
-    const uri = 'http://localhost:3000';
+
+
     const handleClickButton = () =>{
         fetch(`${uri}/coworksInPriceRange`, {
             method: 'GET',
