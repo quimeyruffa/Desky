84be61c95ca0084552deb49b8e94b5523a00c3f6
Juli <julian_gamietea@hotmail.com>
2021-11-12 17:58:30 -0300

    filtrosActualizados

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 const mongoose = require("mongoose");
 var router = express.Router();
-const CoworkController = require('../controllers/cowork.controller');
+const OficinaController = require('../controllers/oficina.controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get("/coworksInPriceRange", CoworkController.getCoworksInPriceRange);
-router.get("/coworksInDateRange", CoworkController.getCoworksInDateRange);
-router.get("/coworksByRecommendations", CoworkController.getCoworksInOrderByRecommendation);
-router.get("/coworks", CoworkController.getAllCoworks);
+router.get("/oficinasInPriceRange", OficinaController.getCoworksInPriceRange);
+router.get("/oficinasInDateRange", OficinaController.getCoworksInDateRange);
+router.get("/oficinasByRecommendations", OficinaController.getCoworksInOrderByRecommendation);
+router.get("/oficinas", OficinaController.getAllCoworks);
 
 module.exports = router;
