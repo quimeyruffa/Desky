3f19404fe84f1c553e864de749171a25f845a16f
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2021-11-23 17:52:10 -0300

    Calendar arreglado

--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -1,14 +1,16 @@
-import { gapi } from 'gapi-script'
-import { useEffect, useState } from "react";
+import {gapi} from 'gapi-script'
+import {useEffect, useState} from "react";
 
 
-
-function Calendar({ id_coworking, className }) {
+function Calendar({id_coworking, className}) {
     const [nombreCowork, setNombreCowork] = useState("");
     const user_email = localStorage.getItem('email');
-    const fechaLlegada = new Date(localStorage.getItem('fechaIni'))
-    const fechaSalida = new Date(localStorage.getItem('fechaFin'))
+    let fechaLlegada = new Date(localStorage.getItem('fechaIni'))
+    let fechaSalida = new Date(localStorage.getItem('fechaFin'))
     const [email, setEmail] = useState("");
+
+    // fechaLlegada = `'${fechaLlegada.toISOString().substring(0, 19)}-07:00'`;
+    // fechaSalida = `'${fechaSalida.toISOString().substring(0, 19)}-07:00'`;
 
     var CLIENT_ID = "1019694152170-7iar05s1ad0luhu9p14jvmg966uskb9f.apps.googleusercontent.com"
     var API_KEY = "AIzaSyASJOlcqh_3DsmhTQKM2k9XiRb25wrdGnk"
@@ -17,16 +19,16 @@ function Calendar({ id_coworking, className }) {
 
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch(`http://localhost:8080/cowork?id=${id_coworking}`, { method: 'GET' });
+            const response = await fetch(`http://localhost:8080/cowork?id=${id_coworking}`, {method: 'GET'});
             const dataCowork = await response.json();
             setEmail(dataCowork[0].email);
             setNombreCowork(dataCowork[0].nombre);
-            console.log("fecha",fechaLlegada.toISOString().substring(0,19));
             console.log(dataCowork[0].email, dataCowork[0].nombre, fechaLlegada.toISOString(), fechaSalida.toISOString(), user_email)
         }
+
         fetchData();
-      }, []);
-    
+    }, []);
+
     const handleClick = () => {
 
         gapi.load('client:auth2', () => {
@@ -47,26 +49,26 @@ function Calendar({ id_coworking, className }) {
                         'summary': `Solicitud de reserva ${nombreCowork}`,
                         'description': `El usuario ${user_email} realizó una reserva para las siguientes fechas`,
                         'start': {
-                            'dateTime': `${fechaLlegada.toISOString().substring(0,19)}-07:00`,
+                            'dateTime': `${fechaLlegada.toISOString().substring(0, 19)}-07:00`,
                             'timeZone': 'America/Argentina/Buenos_Aires'
                         },
-                        'end': {         
-                            'dateTime': `${fechaSalida.toISOString().substring(0,19)}-07:00`,
+                        'end': {
+                            'dateTime': `${fechaSalida.toISOString().substring(0, 19)}-07:00`,
                             'timeZone': 'America/Argentina/Buenos_Aires'
                         },
                         'recurrence': [
                             'RRULE:FREQ=DAILY;COUNT=1'
                         ],
                         'attendees': [
-                            { 'email': `${email}` },
-                            { 'email': `${user_email}` }
+                            {'email': `${email}`},
+                            {'email': `${user_email}`}
                         ],
                         'sendNotifications': true,
                         'reminders': {
                             'useDefault': false,
                             'overrides': [
-                                { 'method': 'email', 'minutes': 24 * 60 },
-                                { 'method': 'popup', 'minutes': 10 }
+                                {'method': 'email', 'minutes': 24 * 60},
+                                {'method': 'popup', 'minutes': 10}
                             ]
                         }
                     }
@@ -83,7 +85,6 @@ function Calendar({ id_coworking, className }) {
         })
     }
 
-    
 
     return (
         <button className={className} onClick={handleClick}>Reservar</button>
