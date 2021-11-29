8de38d2f74aa1b11803a9d2b91671e80a92fcfaa
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2021-11-29 13:16:03 -0300

    Funcionalidad confirmar reservas creada

--- frontend/src/components/Bookings/Cards/Cards.js
+++ frontend/src/components/Bookings/Cards/Cards.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
-import { Button } from './Button';
+import {Button} from './Button';
+import {getEvento} from "../../Calendar/GetEvents";
+
 export default function Cards({
-    coworking_name, coworking_direccion,
-    coworking_price, coworking_date,
-    coworking_amenities,
-    coworking_img,
-    state
-}) {
+                                  coworking_name, coworking_direccion,
+                                  coworking_price, coworking_date,
+                                  coworking_amenities,
+                                  coworking_img,
+                                  state,
+                                  id_objeto,
+                                  fecha_Ini,
+                                  eventos
+                              }) {
+    const [hecho, setHecho] = useState(false);
 
     const newPayment = async () => {
         try{
@@ -18,12 +24,34 @@ export default function Cards({
         }catch(err){
             console.log(err)
         }
-       
+
     }
+    useEffect(async () => {
+        await eventos.forEach((event) => {
+            if (event.start.dateTime === `${fecha_Ini}T00:00:00-03:00`) {
+                if (event.attendees[1].email !== localStorage.getItem("email")) {
+                    if (event.attendees[1].responseStatus !== "needsAction") {
+                        fetch(`http://localhost:8080/reservaConfirmada?id=${id_objeto}`, {method: 'POST'}).then(() => {
+                            console.log("actualizado! 1");
+                            setHecho(true);
+                        });
+                    }
+                } else {
+                    if (event.attendees[0].responseStatus !== "needsAction") {
+                        fetch(`http://localhost:8080/reservaConfirmada?id=${id_objeto}`, {method: 'POST'}).then(() => {
+                            console.log("actualizado! 0");
+                            setHecho(true);
+                        });
+                    }
+                }
+            }
+        })
+    }, [])
 
     return (
+
         <Card>
-            <Img img={coworking_img} />
+            <Img img={coworking_img}/>
             <ContainerData>
                 <Div>
                     <div>
@@ -49,9 +77,11 @@ export default function Cards({
                     </div>
 
                     <div>
-                        {!state ?
-                        <Button width={"100%"} padding={1} text={'Pendiente Confirmacion'} color={'white'} background={'#E0610E'} acction={''} /> :
-                        <Button width={"100%"} padding={1} text={'Confirmado'} color={'white'} background={'#8DB600'} acction={''} />
+                        {!state && !hecho ?
+                            <Button width={"100%"} padding={1} text={'Pendiente Confirmacion'} color={'white'}
+                                    background={'#E0610E'} acction={''}/> :
+                            <Button width={"100%"} padding={1} text={'Confirmado'} color={'white'}
+                                    background={'#8DB600'} acction={''}/>
                         }
 
                     </div>
@@ -62,42 +92,43 @@ export default function Cards({
 }
 
 const Card = styled.div`
-    margin-bottom:0.7em;
-    display:flex;
-    flex-direction:row;
-    width:90%;    
-    border: 1px solid #B9B7B7;
-    border-radius:20px;
-    background-color: #FFFFFF;
-    
+  margin-bottom: 0.7em;
+  display: flex;
+  flex-direction: row;
+  width: 90%;
+  border: 1px solid #B9B7B7;
+  border-radius: 20px;
+  background-color: #FFFFFF;
+
 `
 const Img = styled.div`
-    background-image:url(${props => props.img});
-    width:388px;
-    border-radius:20px 0px 0px 20px;
-    background-size:cover;                    
-    background-repeat:no-repeat;
-    background-position:center center;
+  background-image: url(${props => props.img});
+  width: 388px;
+  border-radius: 20px 0px 0px 20px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
 `
 const ContainerData = styled.div`
-    padding:1em;
-    display:flex;
-    flex-direction:row;
-    width:1295px;
+  padding: 1em;
+  display: flex;
+  flex-direction: row;
+  width: 1295px;
 `
 const Div = styled.div`
-    display:flex;
-    width:50%;
-    flex-direction:column;
-    div{
-        height:40px;
-        display:flex;
-        flex-direction:row;
-    }
+  display: flex;
+  width: 50%;
+  flex-direction: column;
+
+  div {
+    height: 40px;
+    display: flex;
+    flex-direction: row;
+  }
 `
 const P = styled.p`
-    font-size:35px;
-    font-weight:bold;
-    font-family:'Alegreya Sans',sans-serif;
-    color:black;    
+  font-size: 35px;
+  font-weight: bold;
+  font-family: 'Alegreya Sans', sans-serif;
+  color: black;
 `
